2f65cc1f851fbd378d8c00f87772780963fa6b41
Oscar Robles <drumont988@gmail.com>
2022-09-02 00:07:07 +0200

    tests/context.py included: add required paths to sources to sys path for tests

---
+++ tests/context.py
@@ -0,0 +1,14 @@
+"""
+    Context for tests
+    2022-09-01 23:07
+"""
+import os
+import sys
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, path)
+path = path + '/BankStatement2HomeBank/HomeBankCategories'
+sys.path.insert(0, path)
+
+
+import HomeBankCategories
