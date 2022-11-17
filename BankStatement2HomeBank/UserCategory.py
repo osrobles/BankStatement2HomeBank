12bda91cc27c81a0cf50e583b54ac2a3bc5b38de
Oscar Robles <drumont988@gmail.com>
2022-11-18 00:03:29 +0100

    UserCategory (__init__): CSV string as input

--- BankStatement2HomeBank/UserCategory.py
+++ BankStatement2HomeBank/UserCategory.py
@@ -4,7 +4,18 @@
     2022-11-08 23:59
 """
 
-from ListManagement import list2csv
+from enum import IntEnum
+import ListManagement as ListMgmt
+
+
+class CSVInfoIndex(IntEnum):
+    """
+    Category CSV Info Index enumerate
+    """
+    TOKEN = 0
+    SIGN = 1
+    CATEGORY = 2
+    TOTAL = 3
 
 
 class UserCategory():
@@ -12,23 +23,35 @@ class UserCategory():
         Class with custom categories information
     """
 
-    def __init__(self, token='', sign='-', category_name=''):
+    def __init__(self, csv_string='', sep=";"):
         """
         !@brief This function creates a new custom categories object
 
-        @param token            - token ID for the category
-        @param category_name    - category name assigned to the token
+        @param csv_string   - CSV string with the category information (token:sign:category_name)
+
+        @param  sep         - separator character
         """
+        if not isinstance(csv_string, str) or len(csv_string) == 0:
+            raise Exception("ERROR: csv_string")
+        else:
+            cat_info = ListMgmt.csv2list(csv_string, ";")
+
+        if len(cat_info) < CSVInfoIndex.TOTAL.value:
+            raise Exception("ERROR: csv_string number of fields")
+
+        token = cat_info[CSVInfoIndex.TOKEN.value]
         if not isinstance(token, str) or len(token) == 0:
             raise Exception("ERROR: token")
         else:
             self.token = token.lower()
 
+        sign = cat_info[CSVInfoIndex.SIGN.value]
         if sign == '-' or sign == '+':
             self.sign = sign
         else:
             raise Exception(f'ERROR: sign {sign} not allowed')
 
+        category_name = cat_info[CSVInfoIndex.CATEGORY.value]
         if not isinstance(category_name, str) or len(category_name) == 0:
             raise Exception("ERROR: category name")
         else:
@@ -52,4 +75,4 @@ class UserCategory():
 
         @return UserCategory CSV string
         """
-        return list2csv(list(self.__dict__.values()), sep)
+        return ListMgmt.list2csv(list(self.__dict__.values()), sep)
--- tests/UserCategory_tests.py
+++ tests/UserCategory_tests.py
@@ -10,30 +10,36 @@ from context import UserCategory
 
 class Test(unittest.TestCase):
 
+    def test_csv_string_empty(self):
+        self.assertRaises(Exception, UserCategory.UserCategory, '')
+
+    def test_csv_string_wrong_number_of_fields(self):
+        self.assertRaises(Exception, UserCategory.UserCategory, 'token;Cat', ';')
+
     def test_wrong_token(self):
-        self.assertRaises(Exception, UserCategory.UserCategory, '', '-', 'Category')
+        self.assertRaises(Exception, UserCategory.UserCategory, ';-;Category', ';')
 
     def test_wrong_sign(self):
-        self.assertRaises(Exception, UserCategory.UserCategory, 'token', 't', 'Category')
+        self.assertRaises(Exception, UserCategory.UserCategory, 'token;t;Category', ';')
 
     def test_wrong_category_name(self):
-        self.assertRaises(Exception, UserCategory.UserCategory, 'Token', '-', '')
+        self.assertRaises(Exception, UserCategory.UserCategory, 'token;-;', ';')
 
     def test_category_object_created(self):
         try:
-            UserCategory.UserCategory('Token', '-', 'Category')
-            UserCategory.UserCategory('Token', '+', 'Category')
+            UserCategory.UserCategory('Token;-;Category', ";")
+            UserCategory.UserCategory('Token;+;Category', ";")
         except Exception:
             self.fail("test_category_object_created")
 
     def test_category_check_mememship(self):
-        cat = UserCategory.UserCategory('Token', '-', 'Category')
+        cat = UserCategory.UserCategory('Token;-;Category', ";")
         self.assertTrue(cat.checkMembership('Token found'))
         self.assertTrue(cat.checkMembership('token not found'))
         self.assertFalse(cat.checkMembership('toke not found'))
 
     def test_toCSV(self):
-        cat = UserCategory.UserCategory('Token', '-', 'Category')
+        cat = UserCategory.UserCategory('Token;-;Category')
         self.assertEqual('token;-;Category', cat.toCSV())
 
 
