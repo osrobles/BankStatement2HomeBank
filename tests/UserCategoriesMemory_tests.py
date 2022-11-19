00967467de38842887dc47d71edba4f2a9e664ac
Oscar Robles <drumont988@gmail.com>
2022-11-20 00:29:54 +0100

    UserCategoriesMemory_tests: file paths as class variables

--- tests/UserCategoriesMemory_tests.py
+++ tests/UserCategoriesMemory_tests.py
@@ -14,63 +14,63 @@ from context import UserCategoriesMemory as ucm
 
 class Test(unittest.TestCase):
 
+    homebank_file_path = 'data/homebank_test_file.xhb'
+    unknown_path = 'data/unknown.csv'
+    test_file_path = 'data/user_categories_mem_test_file.csv'
+    orig_file_path = 'data/user_categories_mem_test_file.csv.orig'
+    update_file_path = 'data/update.csv'
+
     def test_constructor_unavailable_file(self):
-        userCategories = ucm.UserCategoriesMemory('unknown.csv')
-        os.remove('unknown.csv')
+        userCategories = ucm.UserCategoriesMemory(Test.unknown_path)
+        os.remove(Test.unknown_path)
         self.assertEqual(userCategories.status, True)
 
     def test_constructor_correct_file(self):
-        userCategories = ucm.UserCategoriesMemory('data/user_categories_mem_test_file.csv')
+        userCategories = ucm.UserCategoriesMemory(Test.test_file_path)
         self.assertEqual(userCategories.status, False)
         self.assertEqual(len(userCategories.income_categories), 4)
         self.assertEqual(len(userCategories.expense_categories), 4)
 
     def test_removeDeprecatedCategories_wrong_HomeBankObject(self):
-        userCat = ucm.UserCategoriesMemory('data/user_categories_mem_test_file.csv')
+        userCat = ucm.UserCategoriesMemory(Test.test_file_path)
         self.assertRaises(Exception, userCat.removeDeprecatedCategories, 'None', ';')
 
     def test_removeDeprecatedCategories(self):
-        copyfile('data/user_categories_mem_test_file.csv',
-                 'data/user_categories_mem_test_file.csv.orig')
-        homeBankCat = hbc.HomeBankCategories('data/homebank_test_file.xhb')
-        userCat = ucm.UserCategoriesMemory('data/user_categories_mem_test_file.csv')
+        copyfile(Test.test_file_path, Test.orig_file_path)
+        homeBankCat = hbc.HomeBankCategories(Test.homebank_file_path)
+        userCat = ucm.UserCategoriesMemory(Test.test_file_path)
         userCat.removeDeprecatedCategories(homeBankCat)
         self.assertEqual(len(userCat.income_categories), 3)
         self.assertEqual(len(userCat.expense_categories), 3)
-        copyfile('data/user_categories_mem_test_file.csv',
-                 'data/updated.csv')
-        copyfile('data/user_categories_mem_test_file.csv.orig',
-                 'data/user_categories_mem_test_file.csv')
-        os.remove('data/user_categories_mem_test_file.csv.orig')
-        userCategories = ucm.UserCategoriesMemory('data/updated.csv')
+        copyfile(Test.test_file_path, Test.update_file_path)
+        copyfile(Test.orig_file_path, Test.test_file_path)
+        os.remove(Test.orig_file_path)
+        userCategories = ucm.UserCategoriesMemory(Test.update_file_path)
         self.assertEqual(userCategories.status, False)
         self.assertEqual(len(userCategories.income_categories), 3)
         self.assertEqual(len(userCategories.expense_categories), 3)
-        os.remove('data/updated.csv')
+        os.remove(Test.update_file_path)
 
     def test_addNew_wrong_UserCategoryObject(self):
-        userCat = ucm.UserCategoriesMemory('data/user_categories_mem_test_file.csv')
+        userCat = ucm.UserCategoriesMemory(Test.test_file_path)
         self.assertRaises(Exception, userCat.addNew, 'None', ';')
 
     def test_addNew_correct(self):
-        copyfile('data/user_categories_mem_test_file.csv',
-                 'data/user_categories_mem_test_file.csv.orig')
-        userCatMem = ucm.UserCategoriesMemory('data/user_categories_mem_test_file.csv')
+        copyfile(Test.test_file_path, Test.orig_file_path)
+        userCatMem = ucm.UserCategoriesMemory(Test.test_file_path)
         userCat = uc.UserCategory('Token9;-;Category9', ';')
         userCatMem.addNew(userCat, ';')
-        copyfile('data/user_categories_mem_test_file.csv',
-                 'data/updated.csv')
-        copyfile('data/user_categories_mem_test_file.csv.orig',
-                 'data/user_categories_mem_test_file.csv')
-        os.remove('data/user_categories_mem_test_file.csv.orig')
-        userCategories = ucm.UserCategoriesMemory('data/updated.csv')
+        copyfile(Test.test_file_path, Test.update_file_path)
+        copyfile(Test.orig_file_path, Test.test_file_path)
+        os.remove(Test.orig_file_path)
+        userCategories = ucm.UserCategoriesMemory(Test.update_file_path)
         self.assertEqual(userCategories.status, False)
         self.assertEqual(len(userCategories.income_categories), 4)
         self.assertEqual(len(userCategories.expense_categories), 5)
-        os.remove('data/updated.csv')
+        os.remove(Test.update_file_path)
 
     def test_findCategory(self):
-        userCategories = ucm.UserCategoriesMemory('data/user_categories_mem_test_file.csv')
+        userCategories = ucm.UserCategoriesMemory(Test.test_file_path)
         self.assertEqual(userCategories.status, False)
         self.assertEqual(userCategories.findCategory("-", "Token2"), "Category2")
         self.assertEqual(userCategories.findCategory("+", "Token2"), "")
