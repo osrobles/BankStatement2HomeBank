2fae8365f89d81d74979d305da65e7659afa4d10
Oscar Robles <drumont988@gmail.com>
2022-11-20 00:12:16 +0100

    UserPaymentModesMemory_tests: file paths as class variables

--- tests/UserPaymentModesMemory_tests.py
+++ tests/UserPaymentModesMemory_tests.py
@@ -13,10 +13,14 @@ from context import UserPaymentModesMemory as umm
 
 class Test(unittest.TestCase):
 
+    unknown_path = 'data/unknown.csv'
     test_file_path = 'data/user_modes_mem_test_file.csv'
+    orig_file_path = 'data/user_modes_mem_test_file.csv.orig'
+    update_file_path = 'data/update.csv'
+
     def test_constructor_unavailable_file(self):
-        userModesMem = umm.UserPaymentModesMemory('unknown.csv')
-        os.remove('unknown.csv')
+        userModesMem = umm.UserPaymentModesMemory(Test.unknown_path)
+        os.remove(Test.unknown_path)
         self.assertEqual(userModesMem.status, True)
 
     def test_constructor_debit_card(self):
@@ -34,24 +38,24 @@ class Test(unittest.TestCase):
         self.assertEqual(userModesMem.modes_memory[4].mode, um.PaymentModes.CREDIT_CARD)
 
     def test_addNew_wrong_UserPaymentModeObject(self):
-        userModesMem = umm.UserPaymentModesMemory('data/user_modes_mem_test_file.csv')
+        userModesMem = umm.UserPaymentModesMemory(Test.test_file_path)
         self.assertRaises(Exception, userModesMem.addNew, 'None', ';')
 
     def test_addNew_correct(self):
-        copyfile('data/user_modes_mem_test_file.csv', 'data/user_modes_mem_test_file.csv.orig')
-        userModesMem = umm.UserPaymentModesMemory('data/user_modes_mem_test_file.csv')
+        copyfile(Test.test_file_path, Test.orig_file_path)
+        userModesMem = umm.UserPaymentModesMemory(Test.test_file_path)
         userMode = um.UserPaymentMode('Token9;1', ';')
         userModesMem.addNew(userMode, ';')
-        copyfile('data/user_modes_mem_test_file.csv', 'data/updated.csv')
-        copyfile('data/user_modes_mem_test_file.csv.orig', 'data/user_modes_mem_test_file.csv')
-        os.remove('data/user_modes_mem_test_file.csv.orig')
-        userModesMem = umm.UserPaymentModesMemory('data/updated.csv')
+        copyfile(Test.test_file_path, Test.update_file_path)
+        copyfile(Test.orig_file_path, Test.test_file_path)
+        os.remove(Test.orig_file_path)
+        userModesMem = umm.UserPaymentModesMemory(Test.update_file_path)
         self.assertEqual(userModesMem.status, False)
         self.assertEqual(len(userModesMem.modes_memory), 11)
-        os.remove('data/updated.csv')
+        os.remove(Test.update_file_path)
 
     def test_findMode(self):
-        userModesMem = umm.UserPaymentModesMemory('data/user_modes_mem_test_file.csv')
+        userModesMem = umm.UserPaymentModesMemory(Test.test_file_path)
         self.assertEqual(userModesMem.status, False)
         self.assertEqual(userModesMem.findMode("Token2"), 2)
         self.assertEqual(userModesMem.findMode("Token3"), 3)
